dae295ccce51dfed07b99ecf4f6a2dc505cfdf9a
Pavel Musitschenko <pavel.musitschenko@hdnet.de>
2019-10-17 14:46:09 +0200

    Setup extension files

---
+++ ext_emconf.php
@@ -0,0 +1,17 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Fastly Extension',
+    'description' => 'Fastly Extension',
+    'category' => 'be',
+    'author' => 'Pavel Musitschenko',
+    'author_email' => 'pavel.musitschenko@hdnet.de',
+    'state' => 'alpha',
+    'clearCacheOnLoad' => true,
+    'version' => '',
+    'constraints' => [
+        'depends' => [],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
