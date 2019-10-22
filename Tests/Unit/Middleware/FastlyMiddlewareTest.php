2d74367ea5418891fe06046a0fd382b87a57a103
Pavel Musitschenko <pavel.musitschenko@hdnet.de>
2019-10-22 13:21:56 +0200

    AUP-186 Implement festing framework and testing script

---
+++ Tests/Unit/Middleware/FastlyMiddlewareTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Pavel\CdnFastly\Tests\Unit\Middleware;
+
+
+class FastlyMiddlewareTest extends \Nimut\TestingFramework\TestCase\UnitTestCase
+{
+    public function testCheck(): void
+    {
+
+        $this->assertEquals(true, true);
+    }
+}
