61e941d25ab53fe4e73c6498a7e59b46a383fbce
Pavel Musitschenko <pavel.musitschenko@hdnet.de>
2019-10-31 11:42:42 +0100

    AUP-173 Configure Cache, implement FastlyClearCache.png

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,3 +3,16 @@
 defined('TYPO3_MODE') || die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions'][] = \Pavel\CdnFastly\Hooks\FastlyClearCache::class . '->FastlyClearCache';
+
+$registry->registerIcon('extension-fastly-clearcache', \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class, [
+    'source' => 'EXT:cdn_fastly/Resources/Public/Icons/Cache/FastlyClearCache.png',
+]);
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['CdnFastly'] = [
+    'backend' => Pavel\CdnFastly\Cache\FastlyBackend::class,
+    'groups' => [
+        'fastly',
+        'pages',
+        'news',
+    ],
+];
