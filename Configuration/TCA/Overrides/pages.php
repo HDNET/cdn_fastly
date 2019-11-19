aaddc4035b384bdf3f67e334ed26b8140a15dda2
Tim Lochmüller <tim.lochmueller@hdnet.de>
2019-11-19 13:44:36 +0100

    !!! Drop wrong EXT:autoloader & EXT:site code

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -2,16 +2,7 @@
 
 declare(strict_types=1);
 
-use HDNET\Autoloader\Utility\ModelUtility;
-use HDNET\Site\Service\TcaService;
-
-$GLOBALS['TCA']['pages'] = ModelUtility::getTcaOverrideInformation('cdn_fastly', 'pages');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-    'cdn_fastly',
-    'Resources/Private/TsConfig/Main/Root.ts',
-    'T3M Main'
-);
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 $GLOBALS['TCA']['pages']['columns']['fastly'] = [
     'exclude' => 1,
@@ -28,4 +19,4 @@ $GLOBALS['TCA']['pages']['columns']['fastly'] = [
     ],
 ];
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages', 'social_menu_icon,menu_cols,fastly');
+ExtensionManagementUtility::addToAllTCAtypes('pages', 'fastly');
