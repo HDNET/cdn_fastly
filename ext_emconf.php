fc3c40d1d33c43fb0dc8b52a74b1d97016af0fa4
Christopher Olhöft <christopher.olhoeft@hdnet.de>
2025-12-23 11:53:10 +0100

    refactor: upgrade versions in extension to v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,10 +6,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Pavel Musitschenko',
     'author_email' => 'pavel.musitschenko@hdnet.de',
     'state' => 'beta',
-    'version' => '0.3.0',
+    'version' => '0.4.0',
     'constraints' => [
         'depends' => [
-            'php' => '8.4.0-8.4.16',
+            'php' => '8.0.0-8.4.16',
             'typo3' => '13.4.0-13.4.99',
         ],
     ],
